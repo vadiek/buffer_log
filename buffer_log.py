d368d4fe501ee572b5146a46c3c8f6261e03c3d6
Veslo4123 <68753534+vadiek@users.noreply.github.com>
2022-10-04 21:05:38 +0300

    Update buffer_log.py

--- buffer_log.py
+++ buffer_log.py
@@ -1,15 +1,29 @@
 import pyperclip as pc
 import keyboard as k
+import tkinter as tk
 
+def main():
+    lab.configure(text='Начали')
+    try:
+        while True:
+            f = open('buffer.txt', 'a')
+            k.wait("ctrl+v")
+            print('1')
+            x = pc.paste()
+            f.write(x + '\n')
+            f.close
 
-try:
-    while True:
-        f = open('buffer.txt', 'a')
-        k.wait("ctrl+v")
-        print('1')
-        x = pc.paste()
-        f.write(x + '\n')
+    except KeyboardInterrupt:
         f.close
 
-except KeyboardInterrupt:
-    f.close
+
+win = tk.Tk()
+win.geometry('400x250')
+
+lab = tk.Label(win, text='Лог буфера обмена')
+btn = tk.Button(win, text='Начать', command=main)
+
+lab.grid(column=2, row=2)
+btn.grid(column=2, row=4)
+
+win.mainloop()
